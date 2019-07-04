d23cbeb9fc25eac4f7a7ea18bee24dce2ffb8303
Grant Richmond <mail@grant.codes>
2019-07-05 00:31:14 +0200

    fix featured image label

--- src/components/Field/Featured.jsx
+++ src/components/Field/Featured.jsx
@@ -3,6 +3,6 @@ import FileField from './_File'
 
 const Featured = props => <FileField {...props} accept="image/*" />
 
-Featured.defaultProps = { property: 'featured', label: 'Audio Files' }
+Featured.defaultProps = { property: 'featured', label: 'Featured Image' }
 
 export default Featured
