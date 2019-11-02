cc11cdf5005f0120058e0364062dcee095f46fab
Grant Richmond <mail@grant.codes>
2019-11-02 11:48:32 +0100

    make react & react-dom webpack externals

--- webpack.config.js
+++ webpack.config.js
@@ -28,6 +28,20 @@ let options = () => ({
   optimization: {
     minimize: true,
   },
+  externals: {
+    react: {
+      root: 'React',
+      commonjs2: 'react',
+      commonjs: 'react',
+      amd: 'react',
+    },
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom',
+    },
+  },
 })
 
 module.exports = options
